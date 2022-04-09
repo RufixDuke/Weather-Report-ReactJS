82993aad7078a877fbe88c87fa02c1081c15a442
RufixDuke <rufixduke01@gmail.com>
2022-04-09 14:01:31 +0100

    First Commit

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -1,34 +1,36 @@
 import React, { Fragment } from 'react'
 
-// import classes from './DisplayWeather.module.css'
+import classes from './DisplayWeather.module.css'
 
 const DisplayWeather = ({ display }) => {
+    var f = display.main.temp
+    let a = f - 273.15;
     return (
         <Fragment>
             <div>
-                <div>
-                    <div id="location"><i className="fas fa-map-marker-alt"></i>
+                <div className={classes.output}>
+                    <div className={classes.location}><i className="fas fa-map-marker-alt"></i>
                         {display.name}
                     </div>
-                    <div id="res">
+                    <div className={classes.res}>
                         <img src={`http://openweathermap.org/img/wn/${display.weather[0].icon}@2x.png`} alt="weather icon" />
-                        <div id="data">
-                            <div id="upper">
-                                <h1>{display.main.temp.toFixed(0)}</h1>
+                        <div className={classes.data}>
+                            <div className={classes.upper}>
+                                <h1>{a.toFixed(1)}</h1>
                                 <p>°C</p>
                             </div>
-                            <div id="lower">
+                            <div className={classes.lower}>
                                 <p>{display.weather[0].description}</p>
                             </div>
                         </div>
                     </div>
 
-                    <div className="info">
+                    <div className={classes.info}>
                         <p><i className="fas fa-tint"></i> Humidity: ${display.main.humidity}%</p>
                         <p> <i className="fas fa-arrow-up"></i> <i className="fas fa-arrow-down"></i>
-                            Pressure:  ${display.main.pressure} in</p>
-                        <p> <i className="fas fa-wind"></i> Wind: ${display.wind.speed} mph</p>
-                        <p> <i className="fas fa-eye"></i> Visibility: ${display.visibility}</p>
+                            Pressure:  {display.main.pressure} in</p>
+                        <p> <i className="fas fa-wind"></i> Wind: {display.wind.speed} mph</p>
+                        <p> <i className="fas fa-eye"></i> Visibility: {display.visibility}</p>
                     </div>
                 </div>
             </div>
--- src/container/Wheather.js
+++ src/container/Wheather.js
@@ -1,21 +1,25 @@
 import React, { useState, useEffect, Fragment } from 'react'
 
 import DisplayWeather from '../components/DisplayWeather';
+import classes from '../components/DisplayWeather.module.css'
 
 const Wheather = () => {
     const [query, setQuery] = useState("");
     const [display, setDisplay] = useState('');
-    const [output, setOutput] = useState("london");
+    const [output, setOutput] = useState("");
 
     useEffect(() => {
-        const timer =
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${output}&appid=de91c982f0db3cc64f7ae493e2a70b93`)
-                .then(result => result.json())
-                .then(res => {
-                    console.log(res)
-                    setDisplay(res)
-                })
-                .then(() => clearTimeout(timer))
+        const fetchLocation = () => fetch(`https://api.openweathermap.org/data/2.5/weather?q=${output}&appid=de91c982f0db3cc64f7ae493e2a70b93`)
+            .then(result => result.json())
+            .then(res => {
+                console.log(res)
+                res.cod >= 400
+                    ? setDisplay(res.message || "")
+                    : setDisplay(res)
+            })
+            .catch(e => console.log(e))
+        output && fetchLocation()
+
     }, [output])
 
     const inputHandler = (e) => setQuery(e.target.value);
@@ -25,15 +29,19 @@ const Wheather = () => {
 
     return (
         <Fragment>
-            <div>
-                <form></form>
-                <input type="text" value={query} onChange={inputHandler} />
-                <button type='submit' onClick={buttonHandler}>Search</button>
+            <div className={classes.Display}>
+                <div className={classes.input}>
+                    <form></form>
+                    <input type="text" value={query} onChange={inputHandler} />
+                    <button type='submit' className={classes.btn} onClick={buttonHandler}>
+                        <i className="fas fa-search">Search</i></button>
+                </div>
+
+                <div>
+                    {display instanceof Object ? <DisplayWeather  {...{ display }} /> : display}
+                </div>
             </div>
 
-            <div>
-                {display && <DisplayWeather  {...{ display }} />}
-            </div>
 
         </Fragment>
     )
